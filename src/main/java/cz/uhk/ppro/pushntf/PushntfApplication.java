f8270087794d64a001b48aecc87543fad81f0522
Jiří Pipek <jpipek@trask.cz>
2022-11-16 12:13:09 +0100

    init commit, init spring project

---
+++ src/main/java/cz/uhk/ppro/pushntf/PushntfApplication.java
@@ -0,0 +1,13 @@
+package cz.uhk.ppro.pushntf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushntfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PushntfApplication.class, args);
+	}
+
+}
